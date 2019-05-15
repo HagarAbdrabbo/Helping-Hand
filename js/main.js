b7499476b45a5c818a57119ed12a6ba7c6640076
Hagar Abdrabbo <eaahhmar@students.eaaa.dk>
2019-05-16 01:43:19 +0200

    the dropdown menu

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("Document ready!");
 
 });
+
+
+// drop down menue for the mobile virsion.
+let burger = document.querySelector("#burger-menu");
+let nav = document.querySelector("#mobile-nav");
+let disapper = document.querySelector("#main")
+
+burger.addEventListener("click", () => {
+    nav.classList.add("visible");    
+     console.log("Nav appered");
+      });
+   
+disapper.addEventListener("click", () => {
+    nav.classList.remove("visible");    
+     console.log("Nav disappered");
+      });
+   
