95fb638f7be0e3174abe9ad8fac129aecdba62a7
Hagar Abdrabbo <eaahhmar@students.eaaa.dk>
2019-05-16 14:08:55 +0200

    M- donation & volunteer sections

--- js/main.js
+++ js/main.js
@@ -9,7 +9,11 @@ document.addEventListener("DOMContentLoaded", function() {
 // drop down menue for the mobile virsion.
 let burger = document.querySelector("#burger-menu");
 let nav = document.querySelector("#mobile-nav");
-let disapper = document.querySelector("#main")
+let disapper = document.querySelector("#main");
+let volunteer = document.querySelector("#volunteer");
+let vsection = document.querySelector("#m-volunteer-section");
+let donate = document.querySelector("#donate");
+let dsection = document.querySelector("#m-donation-section");
 
 burger.addEventListener("click", () => {
     nav.classList.add("visible");    
@@ -21,3 +25,35 @@ disapper.addEventListener("click", () => {
      console.log("Nav disappered");
       });
    
+// Mobile donation section.
+
+
+
+donate.addEventListener("click", () => {
+    dsection.classList.add("visible");
+    nav.classList.remove("visible");
+    vsection.classList.remove("visible");  
+     console.log("Nav appered");
+      });
+   
+disapper.addEventListener("click", () => {
+    dsection.classList.remove("visible");
+     console.log("Nav disappered");
+      });
+
+
+// Mobile volunteer section.
+
+
+
+volunteer.addEventListener("click", () => {
+    vsection.classList.add("visible");
+    nav.classList.remove("visible");
+    dsection.classList.remove("visible");
+     console.log("Nav appered");
+      });
+   
+disapper.addEventListener("click", () => {
+    vsection.classList.remove("visible");    
+     console.log("Nav disappered");
+      });
